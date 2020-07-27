f3f405e2b24b2b6f431ac7384339e5aa3a84ed48
seung <seungmin3837@naver.com>
2020-07-27 17:15:13 +0900

    Make C# program

---
+++ program/program/View/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // 프로그램의 상단바 및 테두리를 없애는 코드
+            //this.FormBorderStyle = FormBorderStyle.None;
+
+
+        }
+    }
+}
